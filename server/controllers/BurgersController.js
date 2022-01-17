26a3159509015265fa5832f048a2575ceff82cd5
Owen Manmano <Bigowen27@gmail.com>
2022-01-17 00:40:12 -0700

    ppush

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -1,1 +1,58 @@
+import { burgersService } from "../services/BurgersService"
 import BaseController from '../utils/BaseController'
+ export class BurgersController extends BaseController {
+   constructor(){
+     super('api/burgers')
+     this.router
+      .get('',this.getAll)
+      .get('/:id', this.getById)
+      .post('', this.create)
+      .put('/:id', this.edit )
+      .delete('/:id', this.remove )
+   }
+   async getById(req, res, next ) {
+     try {
+       const burger = await burgersService.getById(req.params.id)
+       return res.send(burger)
+     } catch (error) {
+       next(error)
+     }
+   }
+
+   async getAll(req, res, next) {
+     try {
+       const burger = await burgersService.getAll()
+       return res.send(burger)
+     } catch (error) {
+       new(error)
+     }
+   }
+
+  async create(res, req, next) {
+    try {
+      const burger = await burgersService.edit(req.body)
+      return res.send(burger)
+    } catch (error) {
+      new(error)
+    }
+  }
+  
+  async edit (res, req, next) {
+    try {
+      req.body.id = req.params.id
+      const burger = await burgersService.edit(req.body)
+      return res.send(burger)
+    } catch (error) {
+      new(error)
+    }
+  }
+
+  async remove (req, res, next) {
+    try {
+      await burgersService.remove(req.params.id)
+      res.send({message: 'Deleted'})
+    } catch (error) {
+      new(error)
+    }
+  }
+ }
